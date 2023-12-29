f6676304ddd51d73b419a1680aa437306e6d9d10
ducky961 <gggtop961@gmail.com>
2023-12-29 17:18:27 +0900

    learn variable

---
+++ Inflearn_edu/Assets/Scripts/Example/VariableExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableExample : MonoBehaviour
+{
+    // 변수 : 값을 담는 그릇
+    // 자료형 : 그 그릇의 모양
+
+    // 변수의 선언 -> 자료형 식별자;
+    // 변수의 초기화 -> 자료형 식별자 = 값;
+    int a;
+
+    void Start()
+    {
+        a = 1;
+        Debug.Log(a);
+    }
+}
