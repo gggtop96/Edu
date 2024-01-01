90100a646fa6cac073e67e9c6f89c95f7516c074
ducky961 <gggtop96@gmail.com>
2024-01-01 23:12:27 +0900

    learn Array(1)

---
+++ Inflearn_edu/Assets/Scripts/Example/ArrayExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arro : MonoBehaviour
+{
+    // 배열 선언
+    // 자료형 [] 식별자 = new 자료형[배열의 크기];
+    // 배열의 크기 = 배열 요소의 갯수
+
+    int score1 = 100;
+    int score2 = 200;
+    int score3 = 300;
+
+    int [] scores = new int [3]; // [0,0,0]
+
+    // 배열 초기화
+    // 1. 자료형[] 식별자 = new 자료형[(배열의 크기)] {값1, 값2, 값3...};
+    // 2. 자료형[] 식별자 = {값1, 값2, 값3...};
+    int[] scores2 = new int[]{100, 200, 300}; // [100, 200, 300]
+    int[] scores3 = {100, 200, 300}; // [100,200,300]
+
+}
