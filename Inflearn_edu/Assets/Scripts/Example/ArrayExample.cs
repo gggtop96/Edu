1e81e1c643076911ff0580ff05cb36aa103e8759
ducky961 <gggtop96@gmail.com>
2024-01-02 22:35:30 +0900

    learn Array(2)

--- Inflearn_edu/Assets/Scripts/Example/ArrayExample.cs
+++ Inflearn_edu/Assets/Scripts/Example/ArrayExample.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arro : MonoBehaviour
+public class ArrayExample : MonoBehaviour
 {
     // 배열 선언
     // 자료형 [] 식별자 = new 자료형[배열의 크기];
@@ -20,4 +20,21 @@ public class Arro : MonoBehaviour
     int[] scores2 = new int[]{100, 200, 300}; // [100, 200, 300]
     int[] scores3 = {100, 200, 300}; // [100,200,300]
 
+    // 배열의 사용
+    // 1. 배열 요소 읽기
+    // -> 배열의_이름[index]
+    // *** index는 0부터 시작. ***
+    // 2. 배열 요소 값 대입
+    // -> 배열의_이름[index] = 값;
+    void Start()
+    {
+        // 배열 요소 읽기
+        Debug.Log(scores3[0]);
+        Debug.Log(scores3[1]);
+        Debug.Log(scores3[2]);
+
+        // 배열 요소에 새로운 값 대입
+        scores3 [1] = 50; // scores -> [100, 50, 300]
+         Debug.Log(scores3[1]);
+    }
 }
