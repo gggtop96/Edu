8fc6fda7588e0b4b0217dc068bd2d36aac1501a3
ducky961 <gggtop96@gmail.com>
2024-01-05 23:11:14 +0900

    add2

--- Inflearn_edu/Assets/Scripts/Example/SwitchExample.cs
+++ Inflearn_edu/Assets/Scripts/Example/SwitchExample.cs
@@ -7,7 +7,18 @@ public class SwitchExample : MonoBehaviour
     /*
         switch(조건)
         {
-            
+            case 값1 :
+                조건이 값1과 같을 때 실행할 코드
+            break;
+            case 값2 :
+                조건이 값2와 같을 때 실행할 코드
+            break;
+            case 값3 : 
+                조건이 값3과 같을 때 실행할 코드
+            break;
+            default : 
+                값1,2,3 이 아닐때 실행할 코드
+            break;
         }
     */
 }
