8ac687434f35ce95ebdff27ff34a44e7efc6daef
ducky961 <85559825+gggtop96@users.noreply.github.com>
2024-01-10 14:54:52 +0900

    learn For

---
+++ Inflearn_edu/Assets/Scripts/Example/ForExample.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForExample : MonoBehaviour
+{
+    /*
+    for(초기화;조건식;반복자)
+    {
+        반복할 코드
+    }
+    */
+
+    void Start()
+    {
+        // 0부터 9까지 출력하는 코드
+        // i => 카운터 변수
+        for(int i = 0; i < 10; i++)
+        {
+            Debug.Log(i);
+        }
+        
+        // 0부터 10까지 숫자 중 짝수만 출력하라.
+        for(int i = 0; i <= 10; i += 2)
+        {
+            Debug.Log(i);
+        }
+    }
+}
