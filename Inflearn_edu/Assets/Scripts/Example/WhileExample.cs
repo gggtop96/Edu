be02c4a9e9902c2b03cea22a968b8704ff72f3c6
ducky961 <85559825+gggtop96@users.noreply.github.com>
2024-01-10 16:19:20 +0900

    learn While

---
+++ Inflearn_edu/Assets/Scripts/Example/WhileExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileExample : MonoBehaviour
+{
+   /*
+   While(조건)
+   {
+    반복할 내용
+   }
+   */
+
+   void Start()
+   {
+    // 0부터 9까지 출력
+    int i = 0;
+
+    while(i < 10)
+    {
+        Debug.Log(i);
+        i++;
+    }
+
+    for(int j = 0; j < 10; j++)
+        {
+            Debug.Log(j);
+        }
+   }
+}
